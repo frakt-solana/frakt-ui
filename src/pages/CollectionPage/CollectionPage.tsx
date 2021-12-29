52616fe98be0a1c3c841f4304a7f8c782a00be2f
sablevsky <vladislav1145@gmail.com>
2021-12-29 22:32:29 +0300

    Fixes

--- src/pages/CollectionPage/CollectionPage.tsx
+++ src/pages/CollectionPage/CollectionPage.tsx
@@ -1,12 +1,10 @@
 import { FC, useMemo, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { useHistory } from 'react-router';
 import { useWallet } from '@solana/wallet-adapter-react';
 
 import { Container } from '../../components/Layout';
 import { AppLayout } from '../../components/Layout/AppLayout';
 import { ArrowDownSmallIcon, DiscordIcon, TwitterIcon } from '../../icons';
-import { CollectionData } from '../../utils/collections/collections.model';
 import { useFraktion, VaultState } from '../../contexts/fraktion';
 import styles from './styles.module.scss';
 import { getCollectionThumbnailUrl } from '../../utils';
@@ -18,6 +16,7 @@ import { ControlledSelect } from '../../components/Select/Select';
 import { ControlledToggle } from '../../components/Toggle/Toggle';
 import { mapVaultsByCollectionName } from '../../utils/collections';
 import { VaultsList } from '../../components/VaultsList';
+import { useParams } from 'react-router-dom';
 
 const SORT_VALUES = [
   {
@@ -60,20 +59,11 @@ const CollectionPage: FC = () => {
   const showTradableVaults = watch('showTradableVaults');
 
   const { connected, publicKey } = useWallet();
-  const history = useHistory();
   const [searchString, setSearchString] = useState<string>('');
-  const queryId = history.location.pathname.replace('/collection/', '');
-  const { collectionsItem } = useCollectionsItem(queryId);
+  const { collectionName } = useParams<{ collectionName: string }>();
+  const { collectionsItem: collectionData } =
+    useCollectionsItem(collectionName);
   const { vaults, loading } = useFraktion();
-
-  const {
-    bannerPath,
-    collectionName,
-    website,
-    discord,
-    twitter,
-    thumbnailPath,
-  } = collectionsItem as CollectionData;
 
   const vaultsByCollectionName = useMemo(() => {
     return loading ? {} : mapVaultsByCollectionName(vaults);
@@ -84,7 +74,7 @@ const CollectionPage: FC = () => {
   }, 300);
 
   const userVaults = useMemo(() => {
-    const filteredVaults = vaultsByCollectionName[queryId];
+    const filteredVaults = vaultsByCollectionName[collectionName];
     const [sortField, sortOrder] = sort.value.split('_');
 
     if (filteredVaults) {
@@ -131,7 +121,7 @@ const CollectionPage: FC = () => {
         });
     }
   }, [
-    queryId,
+    collectionName,
     vaultsByCollectionName,
     sort,
     searchString,
@@ -144,30 +134,47 @@ const CollectionPage: FC = () => {
     connected,
     publicKey,
   ]);
+
   return (
     <AppLayout>
       <div className={styles.fullPage}>
         <div
           className={styles.image}
           style={{
-            backgroundImage: `url(${getCollectionThumbnailUrl(bannerPath)})`,
+            backgroundImage: `url(${getCollectionThumbnailUrl(
+              collectionData.bannerPath,
+            )})`,
           }}
         />
       </div>
       <Container component="main" className={styles.container}>
         <div className={styles.banner}>
           <div className={styles.thumbnail}>
-            <img src={getCollectionThumbnailUrl(thumbnailPath)} />
+            <img
+              src={getCollectionThumbnailUrl(collectionData.thumbnailPath)}
+            />
           </div>
-          <div className={styles.title}>{collectionName}</div>
+          <div className={styles.title}>{collectionData.collectionName}</div>
           <div className={styles.socialLinks}>
-            <a href={website} target="_bank" rel="noopener noreferrer">
+            <a
+              href={collectionData.website}
+              target="_bank"
+              rel="noopener noreferrer"
+            >
               <WebsiteIcon width={46} alt="website" />
             </a>
-            <a href={discord} target="_bank" rel="noopener noreferrer">
+            <a
+              href={collectionData.discord}
+              target="_bank"
+              rel="noopener noreferrer"
+            >
               <DiscordIcon width={48} alt="discord" />
             </a>
-            <a href={twitter} target="_bank" rel="noopener noreferrer">
+            <a
+              href={collectionData.twitter}
+              target="_bank"
+              rel="noopener noreferrer"
+            >
               <TwitterIcon width={48} alt="twitter" />
             </a>
           </div>
