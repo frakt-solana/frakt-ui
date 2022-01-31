512bbd583e2e75cc1835b144beef8d73400205eb
sablevsky <vladislav1145@gmail.com>
2022-01-31 15:29:29 +0300

    Fix providing liquidity

--- src/contexts/liquidityPools/liquidityPools.ts
+++ src/contexts/liquidityPools/liquidityPools.ts
@@ -131,7 +131,7 @@ export const addRaydiumLiquidity =
           },
           currencyAmountInA,
           currencyAmountInB,
-          fixedSide: 'b',
+          fixedSide: 'a',
         });
 
       const { blockhash } = await connection.getRecentBlockhash();
