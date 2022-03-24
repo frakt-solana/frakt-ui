f1104b5f468998b09499fcbc1ae0f5dc69d62259
sablevsky <vladislav1145@gmail.com>
2022-03-24 21:26:14 +0300

    Implement new PoolInfo content

--- src/pages/NFTPools/NFTPoolInfoPage/NFTPoolInfoPage.tsx
+++ src/pages/NFTPools/NFTPoolInfoPage/NFTPoolInfoPage.tsx
@@ -1,12 +1,12 @@
-import { useMemo } from 'react';
+import { FC, useMemo } from 'react';
 import { useParams } from 'react-router-dom';
-// import classNames from 'classnames';
+import classNames from 'classnames';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { TokenInfo } from '@solana/spl-token-registry';
 
 import styles from './NFTPoolInfoPage.module.scss';
 import { HeaderInfo } from './components/HeaderInfo';
-// import { SolanaIcon } from '../../../icons';
-// import { POOL_HISTORY_DATA } from './tempData';
-// import { HistoryListItem } from './components/HistoryListItem';
+import { SolanaIcon } from '../../../icons';
 import { usePublicKeyParam } from '../../../hooks';
 import {
   NFTPoolPageLayout,
@@ -19,12 +19,20 @@ import {
 } from '../../../contexts/nftPools';
 import { Loader } from '../../../components/Loader';
 import { useTokenListContext } from '../../../contexts/TokenList';
+import { LinkWithArrow } from '../../../components/LinkWithArrow';
+import { PATHS } from '../../../constants';
+import { Price, useNftPoolTokenBalance, usePoolTokensPrices } from '../hooks';
+import { SELL_COMMISSION_PERCENT } from '../constants';
+import { PoolStats, useCachedPoolsStats } from '../../PoolsPage';
+import { formatNumberWithSpaceSeparator } from '../../../contexts/liquidityPools';
 
 export const NFTPoolInfoPage = (): JSX.Element => {
   const { poolPubkey } = useParams<{ poolPubkey: string }>();
   usePublicKeyParam(poolPubkey);
   useNftPoolsInitialFetch();
   useNftPoolsPolling();
+
+  const { connected } = useWallet();
 
   const { pool, loading: poolLoading } = useNftPool(poolPubkey);
   const poolPublicKey = pool?.publicKey?.toBase58();
@@ -37,7 +45,23 @@ export const NFTPoolInfoPage = (): JSX.Element => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [poolPublicKey, tokensMap]);
 
-  const loading = poolLoading || tokensMapLoading;
+  const {
+    pricesByTokenMint: poolTokenPricesByTokenMint,
+    loading: pricesLoading,
+  } = usePoolTokensPrices([poolTokenInfo]);
+
+  const { poolsStatsByBaseTokenMint, loading: poolsStatsLoading } =
+    useCachedPoolsStats();
+
+  const poolStats = poolsStatsByBaseTokenMint.get(poolTokenInfo?.address);
+
+  const poolTokenPrice = poolTokenPricesByTokenMint.get(poolTokenInfo?.address);
+
+  const { balance: userPoolTokenBalance } = useNftPoolTokenBalance(pool);
+
+  const pageLoading = poolLoading || tokensMapLoading;
+
+  const loading = pricesLoading || poolsStatsLoading;
 
   return (
     <NFTPoolPageLayout
@@ -45,7 +69,7 @@ export const NFTPoolInfoPage = (): JSX.Element => {
         <HeaderInfo
           poolPublicKey={poolPubkey}
           poolTokenInfo={poolTokenInfo}
-          hidden={loading}
+          hidden={pageLoading}
         />
       }
       pageType={PoolPageType.INFO}
@@ -54,77 +78,142 @@ export const NFTPoolInfoPage = (): JSX.Element => {
         <Loader size="large" />
       ) : (
         <div className={styles.root}>
-          <p style={{ width: '100%' }}>*Under development</p>
-          {/* <div className={styles.leftSide}>
-            <div className={styles.priceWrapper}>
-              <h5 className={styles.cardTitle}>Price</h5>
-              <ul className={styles.priceList}>
-                <li
-                  className={classNames(styles.priceItem, styles.priceItemBuy)}
-                >
-                  <p>Buy price</p>
-                  <span>{0.002124}</span>
-                  <SolanaIcon /> SOL
-                </li>
-                <li
-                  className={classNames(styles.priceItem, styles.priceItemMid)}
-                >
-                  <p>mid price</p>
-                  <span>{0.002124}</span>
-                  <SolanaIcon /> SOL
-                </li>
-                <li
-                  className={classNames(styles.priceItem, styles.priceItemSell)}
-                >
-                  <p>sell price</p>
-                  <span>{0.002124}</span>
-                  <SolanaIcon /> SOL
-                </li>
-              </ul>
-            </div>
-            <div className={styles.additionalInfo}>
-              <div className={styles.additionalItem}>
-                <h5 className={styles.cardTitle}>Volume</h5>
-                <p className={styles.additionalContent}>
-                  <span>{0.002124}</span>
-                  <SolanaIcon /> SOL
-                </p>
-              </div>
-              <div className={styles.additionalItem}>
-                <h5 className={styles.cardTitle}>APY</h5>
-                <p className={styles.additionalContent}>
-                  <span>{12.112}</span> %
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className={styles.rightSide}>
-            <h5 className={styles.cardTitle}>Pool history</h5>
-            <ul className={styles.historyList}>
-              <li className={styles.historyHeader}>
-                <div className={styles.historyCell}>
-                  <span>item</span>
-                </div>
-                <div className={styles.historyCell}>
-                  <span>items id</span>
-                </div>
-                <div className={styles.historyCell}>
-                  <span>action</span>
-                </div>
-                <div className={styles.historyCell}>
-                  <span>price</span>
-                </div>
-                <div className={styles.historyCell}>
-                  <span>date</span>
-                </div>
-              </li>
-              {POOL_HISTORY_DATA.map((item, idx) => (
-                <HistoryListItem key={idx} itemData={item} />
-              ))}
-            </ul>
-          </div> */}
+          {!!poolTokenPrice && !!poolTokenInfo && (
+            <PriceSection price={poolTokenPrice} />
+          )}
+
+          {!!poolStats && <LiquiditySection poolStats={poolStats} />}
+
+          {connected && !!poolTokenInfo && (
+            <UserBalanceSection
+              balance={userPoolTokenBalance}
+              tokenInfo={poolTokenInfo}
+            />
+          )}
         </div>
       )}
     </NFTPoolPageLayout>
+  );
+};
+
+interface UserBalanceSectionProps {
+  balance?: number;
+  tokenInfo: TokenInfo;
+}
+
+const UserBalanceSection: FC<UserBalanceSectionProps> = ({
+  balance = 0,
+  tokenInfo,
+}) => {
+  return (
+    <div className={styles.balanceWrapper}>
+      <h5 className={styles.cardTitle}>Your balance</h5>
+      <PriceSOL price={balance?.toFixed(3)} customToken={tokenInfo} />
+    </div>
+  );
+};
+
+interface LiquiditySectionProps {
+  poolStats: PoolStats;
+}
+
+const LiquiditySection: FC<LiquiditySectionProps> = ({ poolStats }) => {
+  return (
+    <div className={styles.liquidityWrapper}>
+      <h5 className={styles.cardTitle}>Liquidity</h5>
+      <p className={styles.liquiditySubtitle}>APR</p>
+      <p>{poolStats?.apr} %</p>
+      <p className={styles.liquiditySubtitle}>Volume</p>
+      <p>$ {formatNumberWithSpaceSeparator(poolStats?.volume || 0)}</p>
+      <LinkWithArrow
+        to={`${PATHS.YIELD}`}
+        label="Earn"
+        className={styles.liquidityLink}
+      />
+    </div>
+  );
+};
+
+interface PriceSectionProps {
+  price: Price;
+}
+
+const PriceSection: FC<PriceSectionProps> = ({ price }) => {
+  const buyPrice = parseFloat(price.buy)?.toFixed(3);
+  const sellPrice = (
+    parseFloat(price.sell) *
+    ((100 - SELL_COMMISSION_PERCENT) / 100)
+  )?.toFixed(3);
+  const swapPrice = (
+    parseFloat(price.buy) *
+    (SELL_COMMISSION_PERCENT / 100)
+  )?.toFixed(3);
+
+  return (
+    <div className={styles.priceWrapper}>
+      <h5 className={styles.cardTitle}>Price</h5>
+      <ul className={styles.priceList}>
+        <li className={classNames(styles.priceItem, styles.priceItemBuy)}>
+          <p
+            className={classNames(
+              styles.priceItemTitle,
+              styles.priceItemTitleBuy,
+            )}
+          >
+            Buy price
+          </p>
+          <PriceSOL price={buyPrice} />
+        </li>
+        <li className={classNames(styles.priceItem, styles.priceItemSell)}>
+          <p
+            className={classNames(
+              styles.priceItemTitle,
+              styles.priceItemTitleSell,
+            )}
+          >
+            Sell price
+          </p>
+          <PriceSOL price={sellPrice} />
+        </li>
+        <li className={classNames(styles.priceItem, styles.priceItemSwap)}>
+          <p
+            className={classNames(
+              styles.priceItemTitle,
+              styles.priceItemTitleSwap,
+            )}
+          >
+            Swap price
+          </p>
+          <PriceSOL price={swapPrice} />
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+interface PriceSOLProps {
+  price: string;
+  customToken?: TokenInfo;
+  className?: string;
+}
+
+const PriceSOL: FC<PriceSOLProps> = ({ price, className, customToken }) => {
+  return (
+    <div className={classNames(styles.solPriceWrapper, className)}>
+      <span className={styles.solPrice}>{price}</span>
+      {customToken ? (
+        <>
+          <div
+            className={styles.tokenPriceIcon}
+            style={{ backgroundImage: `url(${customToken?.logoURI})` }}
+          />{' '}
+          {customToken?.symbol}
+        </>
+      ) : (
+        <>
+          <SolanaIcon className={styles.solPriceIcon} /> SOL
+        </>
+      )}
+    </div>
   );
 };
