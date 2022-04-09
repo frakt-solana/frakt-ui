1c2dbd0e3b54f494e2a41bae3ce6d426a3274ffc
sablevsky <vladislav1145@gmail.com>
2022-04-09 15:02:19 +0400

    Implement feature for instant buyout

--- src/contexts/auction/transactions/createBuyoutByOwner.ts
+++ src/contexts/auction/transactions/createBuyoutByOwner.ts
@@ -1,67 +1,52 @@
-export default {};
+import { createBuyoutByOwner as createBuyoutByOwnerTx } from '@frakters/fraktionalizer-client-library';
 
-// import {} from 'fraktionalizer-client-library';
+import {
+  createTransactionFuncFromRaw,
+  signAndConfirmTransaction,
+  WalletAndConnection,
+} from '../../../utils/transactions';
+import { wrapAsyncWithTryCatch } from '../../../utils';
+import fraktionConfig from '../../fraktion/config';
+import { VaultData } from '../../fraktion';
 
-// import {
-//   createTransactionFuncFromRaw,
-//   signAndConfirmTransaction,
-//   WalletAndConnection,
-// } from '../../../utils/transactions';
-// import { wrapAsyncWithTryCatch } from '../../../utils';
-// import fraktionConfig from '../../fraktion/config';
-// import { VaultData } from '../../fraktion';
+interface CreateBuyoutByOwnerParams {
+  vaultInfo: VaultData;
+}
 
-// interface CreateBuyoutByOwnerParams {
-//   vaultInfo: VaultData;
-//   price: number;
-// }
+interface CreateBuyoutByOwnerRawParams
+  extends CreateBuyoutByOwnerParams,
+    WalletAndConnection {}
 
-// interface CreateBuyoutByOwnerRawParams
-//   extends CreateBuyoutByOwnerParams,
-//     WalletAndConnection {}
+export const rawCreateBuyoutByOwner = async ({
+  wallet,
+  connection,
+  vaultInfo,
+}: CreateBuyoutByOwnerRawParams): Promise<void> => {
+  await createBuyoutByOwnerTx(
+    connection,
+    vaultInfo.vaultPubkey,
+    wallet.publicKey.toBase58(),
+    vaultInfo.fractionMint,
+    vaultInfo.fractionTreasury,
+    vaultInfo.authority,
+    fraktionConfig.PROGRAM_PUBKEY,
+    async (transaction) => {
+      await signAndConfirmTransaction({
+        transaction,
+        connection,
+        wallet,
+      });
+    },
+  );
+};
 
-// export const rawCreateBuyoutByOwner = async ({
-//   wallet,
-//   connection,
-//   vaultInfo,
-//   price,
-// }: CreateBuyoutByOwnerRawParams): Promise<void> => {
-//   const supply = vaultInfo.fractionsSupply.toNumber();
-//   const perShare = Math.ceil((price * 1e9) / supply);
-//   const bidCap = perShare * supply;
+const wrappedAsyncWithTryCatch = wrapAsyncWithTryCatch(rawCreateBuyoutByOwner, {
+  onSuccessMessage: {
+    message: 'Buyout made successfully',
+  },
+  onErrorMessage: { message: 'Transaction failed' },
+});
 
-//   await createBuyoutByOwnerTx({
-//     connection,
-//     winning_bid: vaultInfo.auction.auction.currentWinningBidPubkey,
-//     bidPerShare: perShare,
-//     bidCap,
-//     adminPubkey: fraktionConfig.ADMIN_PUBKEY,
-//     userPubkey: wallet.publicKey,
-//     vault: vaultInfo.vaultPubkey,
-//     auction: vaultInfo.auction.auction.auctionPubkey,
-//     fractionMint: vaultInfo.fractionMint,
-//     fractionTreasury: vaultInfo.fractionTreasury,
-//     redeemTreasury: vaultInfo.redeemTreasury,
-//     priceMint: vaultInfo.priceMint,
-//     vaultProgramId: fraktionConfig.PROGRAM_PUBKEY,
-//     sendTxn: async (transaction, signers) => {
-//       await signAndConfirmTransaction({
-//         transaction,
-//         signers,
-//         connection,
-//         wallet,
-//       });
-//     },
-//   });
-// };
-
-// const wrappedAsyncWithTryCatch = wrapAsyncWithTryCatch(rawCreateBuyoutByOwner, {
-//   onSuccessMessage: {
-//     message: 'Buyout made successfully',
-//   },
-//   onErrorMessage: { message: 'Transaction failed' },
-// });
-
-// export const bidOnAuction = createTransactionFuncFromRaw(
-//   wrappedAsyncWithTryCatch,
-// );
+export const createBuyoutByOwner = createTransactionFuncFromRaw(
+  wrappedAsyncWithTryCatch,
+);
