dabfb93bc412f0161f576b9f5cbc634de055aac6
sablevsky <vladislav1145@gmail.com>
2022-04-12 17:22:36 +0400

    Uncomment hidden nft pools

--- src/utils/cacher/nftPools/nftPools.constants.ts
+++ src/utils/cacher/nftPools/nftPools.constants.ts
@@ -1,6 +1,6 @@
 export const HIDDEN_POOLS = [
-  'CA5qBCJS5wwdvMmt2ANLUxUecmNjf7Py9s7N1UEJ1cxy',
-  'AaYBdEnjqYoay3ozs2tY1tm9kFXxjUrAZeNZngnqwmV1',
-  'HfEJXp9YNdrrPuqAoaeKzL3pyCvsdnVQxHXpf7KVTsjY',
-  '7Bjj5LSjvC6Sumvx3L3gK25AGVGhFfPyZWkm1xep8kXv',
+  // 'CA5qBCJS5wwdvMmt2ANLUxUecmNjf7Py9s7N1UEJ1cxy', //? High Roller Hippo Clique Pool
+  // 'AaYBdEnjqYoay3ozs2tY1tm9kFXxjUrAZeNZngnqwmV1', //? Sollamas pool
+  // 'HfEJXp9YNdrrPuqAoaeKzL3pyCvsdnVQxHXpf7KVTsjY', //? Solpunks pool
+  // '7Bjj5LSjvC6Sumvx3L3gK25AGVGhFfPyZWkm1xep8kXv', //? Sol Patrol pool
 ];
