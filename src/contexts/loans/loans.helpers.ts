5e7b7a0730d81b9108209e5ca977837422144c90
Rustam Tairov <rustairov@gmail.com>
2022-06-09 16:07:57 +0100

    DEVS-297: @frakt-protocol/frakt-sdk verison fix

--- src/contexts/loans/loans.helpers.ts
+++ src/contexts/loans/loans.helpers.ts
@@ -1,53 +1,9 @@
-import {
-  CollectionInfoView,
-  getAllProgramAccounts,
-  LoanView,
-} from '@frakters/nft-lending-v2';
-import { Connection, PublicKey } from '@solana/web3.js';
-import { groupBy, Dictionary } from 'lodash';
-import { LoanData, LoanDataByPoolPublicKey } from '@frakt-protocol/frakt-sdk';
+import { LoanView } from '@frakters/nft-lending-v2';
+import { Dictionary } from 'lodash';
+import { LoanData } from '@frakt-protocol/frakt-sdk';
 
 import { getNftCreators, SOL_TOKEN } from '../../utils';
 import { UserNFT } from '../../state/userTokens/types';
-
-export const fetchLoanDataByPoolPublicKey = async (
-  connection: Connection,
-): Promise<LoanDataByPoolPublicKey> => {
-  const { collectionInfos, deposits, liquidityPools, loans } =
-    await getAllProgramAccounts(
-      new PublicKey(process.env.LOANS_PROGRAM_PUBKEY),
-      connection,
-    );
-
-  const collectionInfosByPoolPublicKey = groupBy(
-    collectionInfos,
-    'liquidityPool',
-  );
-  const depositsByPoolPublicKey = groupBy(deposits, 'liquidityPool');
-  const loansByPoolPublicKey = groupBy(loans, 'liquidityPool');
-
-  return liquidityPools?.reduce((loansData, liquidityPool) => {
-    const { liquidityPoolPubkey } = liquidityPool;
-
-    return loansData.set(liquidityPoolPubkey, {
-      collectionsInfo:
-        collectionInfosByPoolPublicKey[liquidityPoolPubkey] || [],
-      deposits: depositsByPoolPublicKey[liquidityPoolPubkey] || [],
-      liquidityPool: liquidityPool,
-      loans: loansByPoolPublicKey[liquidityPoolPubkey] || [],
-    });
-  }, new Map<string, LoanData>());
-};
-
-export const getLoanCollectionInfo = (
-  loanData: LoanData,
-  collectionInfoPublicKey: string,
-): CollectionInfoView => {
-  return loanData.collectionsInfo?.find(
-    ({ collectionInfoPubkey }) =>
-      collectionInfoPubkey === collectionInfoPublicKey,
-  );
-};
 
 type GetFeePercent = (props: { nft?: UserNFT; loanData: LoanData }) => number;
 
