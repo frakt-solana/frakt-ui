a80cb482bdde3983e63c7ab797937f3e7e422b0d
sablevsky <vladislav1145@gmail.com>
2022-07-18 17:57:53 +0400

    Remove prism

--- src/state/rootReducer.ts
+++ src/state/rootReducer.ts
@@ -3,14 +3,12 @@ import { combineReducers } from 'redux';
 import commonReducers from './common/reducers';
 import tokenListReducers from './tokenList/reducers';
 import loansReducers from './loans/reducers';
-import fetchPrismReducer from './prism/reducers';
 import fetchStatsReducer from './stats/reducers';
 
 const rootReducers = combineReducers({
   common: commonReducers,
   tokenList: tokenListReducers,
   loans: loansReducers,
-  prism: fetchPrismReducer,
   stats: fetchStatsReducer,
 });
 
